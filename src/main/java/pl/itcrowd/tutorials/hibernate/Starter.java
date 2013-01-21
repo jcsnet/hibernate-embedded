0db1d19f98aed717d4012d2f9e3fecf712ef5ee7
lukasz <lukasz.jackowski@itcrowd.pl>
2013-01-21 11:26:25 +0100

    In Starter class we inject EntityManager

--- src/main/java/pl/itcrowd/tutorials/hibernate/Starter.java
+++ src/main/java/pl/itcrowd/tutorials/hibernate/Starter.java
@@ -1,9 +1,21 @@
 package pl.itcrowd.tutorials.hibernate;
 
+import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 
 @Startup
 @Singleton
 public class Starter {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @PostConstruct
+    public void postConstruct()
+    {
+
+    }
 }
