5a047309b34f5bb32e0012b157ef15029a026010
lukasz <lukasz.jackowski@itcrowd.pl>
2013-01-21 11:30:55 +0100

    In method PostConstruct create object class Company, Department, Address and Employee

--- src/main/java/pl/itcrowd/tutorials/hibernate/Starter.java
+++ src/main/java/pl/itcrowd/tutorials/hibernate/Starter.java
@@ -1,5 +1,10 @@
 package pl.itcrowd.tutorials.hibernate;
 
+import pl.itcrowd.tutorials.hibernate.Company.Address;
+import pl.itcrowd.tutorials.hibernate.Company.Company;
+import pl.itcrowd.tutorials.hibernate.Company.Department;
+import pl.itcrowd.tutorials.hibernate.Company.Employee;
+
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
@@ -16,6 +21,13 @@ public class Starter {
     @PostConstruct
     public void postConstruct()
     {
+        final Company company = new Company();
+        Department department1 = new Department();
+        Department department2 = new Department();
+        Address address = new Address();
+        Employee employee1 = new Employee();
+        Employee employee2 = new Employee();
+        Employee employee3 = new Employee();
 
     }
 }
